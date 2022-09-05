ee50e296dca318d3763519f2de5a00724e704514
A.Islamshin <arshavin0511@gmail.com>
2022-09-05 15:32:49 +0300

    Dop2option

--- Dop/Program.cs
+++ Dop/Program.cs
@@ -1,34 +1,38 @@
-﻿int[] numbers = { 6, 2, 356, 9, 7, 91, 64, 0 };
-int length = numbers.Length;
-int index = 0;
-int position = 0;
-int max2 = 0;
-int max = numbers[0];
-while (index < length)
+﻿int[] f = { 5, 2, 356, 9, 71, 0, 649, 0 };
+void Mass(int[] numbers)
 {
-    if (numbers[index] == 0)
+    int length = numbers.Length;
+    int index = 0;
+    int max2 = 0;
+    int max = numbers[0];
+    while (index < length)
     {
-        position = numbers[index];
-        break;
-    }
-    else if (numbers[index] != 0)
-    {
-        for (int i = 0; i < numbers[index]; i++) ;
+        if (numbers[index] == 0)
         {
-            if (numbers[index] > max)
-            {
-                max2 = max;
-                max = numbers[index];
-            }
-            else if (numbers[index] > max2) max2 = numbers[index];
+            break;
         }
+        else 
+        {
+            for (int i = 0; i < numbers[index]; i++) ;
+            {
+                if (numbers[index] > max)
+                {
+                    max2 = max;
+                    max = numbers[index];
+                }
+                else if (numbers[index] > max2) max2 = numbers[index];
+            }
 
 
+
+        }
+        index++;
 
     }
-    index++;
-
+    Console.WriteLine(max2);
 }
+
+
 void PrintMass(int[] col)
 {
     int length = col.Length;
@@ -39,8 +43,6 @@ void PrintMass(int[] col)
         index++;
     }
 }
-PrintMass(numbers);
+PrintMass(f);
 Console.WriteLine("_____________");
-Console.WriteLine(max2);
-
-
+Mass(f);
